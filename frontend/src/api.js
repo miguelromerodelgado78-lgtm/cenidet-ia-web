47597f0aa638cdfd244860d8b343c2747b2d00c9
miguelromerodelgado78-lgtm <miguelromerodelgado78@gmail.com>
2026-03-18 00:17:08 -0600

    Conectando frontend a Render y limpiando archivos

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/'
+    baseURL: window.location.hostname === 'localhost' 
+        ? 'http://127.0.0.1:8000/api/' 
+        : 'https://cenidet-ia-web.onrender.com/api/'
 });
 
 export default API;
